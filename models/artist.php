d95bb76a45349c1d23e4cfce757eec2ee0bedc2c
darkhitman <darkhitman@35c238a6-caef-2181-63be-5a3b3fc4cf75>
2010-10-29 02:38:37 +0000

    Adding artist model.

---
+++ models/artist.php
@@ -0,0 +1,39 @@
+<?php
+
+require_once('include/model.php');
+require_once('include/database.php');
+
+class Artist extends Model 
+{
+	function getAllArtists()
+	{
+		$qry = "SELECT * FROM artists";
+		$res = mysql_query($qry);
+		$res_array = Database::resultToArray($res);
+		if (DEBUG) 
+		{
+			echo "$qry  returned results: <br>"
+			print_r($res_array);			
+		}
+		return $res_array;
+	}
+	
+	function getArtistById($id)
+	{	
+		$qry = "SELECT * FROM artists WHERE id=$id";
+		$res = mysql_query($qry);
+		$res_row = Database::resultToRow($res);
+		if (DEBUG) 
+		{
+			echo "$qry  returned results: <br>";
+			print_r($res_row);
+		}
+		return $res_array;
+	
+	}
+
+}
+
+
+
+?>

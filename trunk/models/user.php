3828ee66ca6c8922fbd65dbca30ca9dd37cc1db5
robert.nubel <robert.nubel@35c238a6-caef-2181-63be-5a3b3fc4cf75>
2010-10-29 03:49:05 +0000

    Added stuff.

--- trunk/models/user.php
+++ trunk/models/user.php
@@ -1,7 +1,7 @@
 <?php
 
-require_once 'includes/model.php'
-require_once 'includes/database.php'
+require_once 'includes/model.php';
+require_once 'includes/database.php';
 
 class User extends Model {
 	function getAll() {
@@ -62,4 +62,37 @@ class User extends Model {
 		return Database::resultToArray($qry);
 
 	}
+
+	function likeGenre($user_id, $genre_id) {
+		$cUserId = (int)$user_id;
+		$cGenreID = (int)$genre_id;
+		$sql = "INSERT INTO users_liking_genres ug (user_id, genre_id) VALUES ($cUserId, $cGenreId)";
+		return mysql_query($sql);
+	}
+
+	function likeArtist($user_id, $artist_id) {
+		$cUserId = (int)$user_id;
+		$cArtistID = (int)$artist_id;
+		$sql = "INSERT INTO users_liking_artists ua (user_id, artist_id) VALUES ($cUserId, $cArtistId)";
+		return mysql_query($sql);
+	}
+
+	function likeAlbum($user_id, $artist_id, $album_name) {
+		$cUserId = (int)$user_id;
+		$cArtistID = (int)$artist_id;
+		$cAlbumName = mysql_real_escape_string($album_name);
+		$sql = "INSERT INTO users_liking_albums ua (user_id, artist_id, album_name) VALUES ($cUserId, $cArtistId, '$cAlbumName')";
+		return mysql_query($sql);
+	}
+
+	function likeSong($user_id, $artist_id, $album_name, $song_name) {
+		$cUserId = (int)$user_id;
+		$cArtistID = (int)$artist_id;
+		$cAlbumName = mysql_real_escape_string($album_name);
+		$cSongName = mysql_real_escape_string($song_name);
+		$sql = "INSERT INTO users_liking_songs us (user_id, artist_id, album_name, song_name) VALUES ($cUserId, $cArtistId, '$cAlbumName', '$cSongName')";
+		return mysql_query($sql);
+	}
+
+
 }

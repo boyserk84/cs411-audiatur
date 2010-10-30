7179970de127c6824ebc4ad8d5be47f6f5533433
robert.nubel <robert.nubel@35c238a6-caef-2181-63be-5a3b3fc4cf75>
2010-10-30 19:21:42 +0000

    Addded form controller

---
+++ includes/form.php
@@ -0,0 +1,31 @@
+<?php
+/* Helper methods to create forms. */
+
+function start_form($action) {
+	echo "<form action='$action'>";
+	echo "<table class='genericForm'>";
+}
+
+function text_field($label, $name, $val=false) {
+	if ($val === false) {
+		// Auto-fill from request?
+		if (array_key_exists($name, $_REQUEST)) {
+			$val = $_REQUEST[$name];
+		}
+	}
+	echo "<tr><td>$label</td>";
+	echo "<td><input type=text name='$name' value='$val'/></td></tr>";
+}
+
+function hidden_field($name, $val) {
+	echo "<input type=hidden name='$name' value='$val'/>";
+}
+
+function submit_buttom($label) {
+	echo "<tr><td colspan=2 align=center><input type=submit value='$label'></td></tr>";
+}
+
+function close_form() {
+	echo "</table>";
+	echo "</form>";
+}

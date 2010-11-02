5ba4eb0ec165820ada73143eeaf9822fffd35945
robert.nubel <robert.nubel@35c238a6-caef-2181-63be-5a3b3fc4cf75>
2010-11-02 04:00:52 +0000

    Added changes to admin form and models.

--- trunk/admin_artists.php
+++ trunk/admin_artists.php
@@ -1,29 +1,67 @@
 <?php
 
 require 'includes/global.php';
+require 'includes/page.php';
+require 'includes/form.php';
+
+require 'models/artist.php';
 
 class EditArtistsPage extends Page {
-	function handleActions() {
-		if (array_key_exists('add', $_POST)) {
-			$errors = Artist::insertFromPost();	
+	function handleActions() {		
+		$errors = array();
+		if (array_key_exists('add', $_POST)) {			
+			$errors = Artist::insertFromPost();				
 		} elseif (array_key_exists('edit', $_POST)) {
 			$errors = Artist::updateFromPost();
 		}
 
 		if (sizeof($errors) > 0) {
-			echo "<div class='errorbox'>Please correct the following errors:<ul>"
+			echo "<div class='errorbox'>Please correct the following errors:<ul>";
 			foreach ($errors as $error) {
 				echo "<li>" . $error;	
 			}
-			echo "</ul></div>
+			echo "</ul></div>";
 		}
 	}
 
-	function content() {
+	function content() {	
+		EditArtistsPage::handleActions();
+	
 		echo "<h1>Manage Artists</h1>";
 
-		handleActions();
-
+		?>
+		<table>
+			<tr>
+				<th>Artist Name</th><th>Founded In</th><th>Description</th><th></th>
+			</tr>
+		<?php
+		
+		$artists = Artist::getAll();
+		foreach ($artists as $artist) {
+			?>
+				<tr>
+					<td>
+						<?php echo $artist['name']; ?>
+					</td>
+					<td>
+						<?php echo $artist['founded_in']; ?>
+					</td>
+					<td>
+						<?php echo substr($artist['description'], 0, 100) . (strlen($artist['description'])>100 ? "..." : ""); ?>
+					</td>
+					<td>
+						<a href='admin_artists.php?edit=<?php echo $artist['id'];?>'>Edit</a>
+					</td>
+				</tr>			
+			<?php		
+		}
+		
+		?>
+		</table>
+		<?php
+		
+		echo "<h1>Add New Artist</h1>";		
+		
 		// Print the add-artist form.
 		start_form('');
 		hidden_field('add', true);
@@ -35,6 +73,10 @@ class EditArtistsPage extends Page {
 
 	}
 }
+
+
+$page = new EditArtistsPage();
+$page->render();
 
 
 
